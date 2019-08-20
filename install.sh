79e832d7d82e3ffc167a22490b7d2ca09e616138
ahmedhanafy725 <hanafya@codescalers.com>
2019-08-20 09:09:39 +0200

    Remove install.sh

--- install.sh
+++
@@ -1,8 +0,0 @@
-set -ex
-apt-get update; apt-get install -y python3.6 curl git locales language-pack-en rsync unzip python3-pip
-mkdir -p /root
-cd /root
-git clone --branch $branch https://github.com/AhmedHanafy725/test.git
-cd test
-git reset --hard $commit
-pip3 install pytest nose black
